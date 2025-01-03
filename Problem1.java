567f96ad9c025c19c7c21dc48a5877c2f2c49219
chinmaygandi <gandi.c@northeastern.edu>
2025-01-02 19:45:06 -0500

    done 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
+public class Problem1 {
 
+    public static int find_missing(int[] nums) {
+        if ((nums.length == 0) || (nums == null)) {
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == mid + 1) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,5};
+        System.err.println(find_missing(nums));
+    }
+}
